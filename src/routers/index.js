2b0f5f0bdc5719f723c49da47ffccf7cdfb68d2d
brucecham <brucecham@qq.com>
2017-04-12 23:32:06 +0800

    a

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,19 +1,21 @@
 import React,{Component} from 'react'
-import { IndexRoute } from 'react-router';
 import {
   BrowserRouter as Router,
   Route,
   hashHistory,
+  browserHistory,
   Redirect
 } from 'react-router-dom'
 
 import counterContainer from '../containers/counterContainer'
-
+import App from '../components/app'
 
 const routes = (
-  <Router history={hashHistory}>
-    <Route path="/" component={counterContainer}>
-    </Route>
+  <Router history={hashHistory} basename='#'>
+    <App>
+      <Route path="/counter" component={counterContainer}>
+      </Route>
+    </App>
   </Router>
   )
 
