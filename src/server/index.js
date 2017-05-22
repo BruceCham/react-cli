8160b900a27b7dd290de39ce97e465965f5325a7
brucecham <brucecham@qq.com>
2017-05-22 10:49:06 +0800

    server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
 import {axios} from '@/utils'
 
 function getSceneInfo (id) {
-  return axios.get('/api/getSceneInfo', {
+  return axios({
+    url: '/api/getSceneInfo',
     params: {
       sceneId: id
     }
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,18 +1,27 @@
 import axios from 'axios'
 
-export default function (url, options) {
-  // options = Object.assign({},options,{credentials: "include"})
-  return axios(url, options).then((response) => {
-    let {headers, data} = response
-    let contentType = headers['content-type']
-    if (contentType && contentType.indexOf('application/json') !== -1) {
-      let {status, message} = data
-      if (status !== 0) {
-        return Promise.reject(new Error(message))
+export default function (options) {
+  let DefaultParams = {
+    url: '',
+    method: 'get',
+    credentials: 'include'
+  }
+  options = Object.assign({}, DefaultParams, options)
+  try {
+    return axios(options).then((response) => {
+      let {headers, data} = response
+      let contentType = headers['content-type']
+      if (contentType && contentType.indexOf('application/json') !== -1) {
+        let {status, message} = data
+        if (status !== 0) {
+          return Promise.reject(new Error(message))
+        }
+        return Promise.resolve(data)
+      } else {
+        return Promise.reject(new Error('the response is not JSON'))
       }
-      return Promise.resolve(data)
-    } else {
-      return Promise.reject(new Error('the response is not JSON'))
-    }
-  })
+    })
+  } catch (e) {
+    console.error('axios error: ', e)
+  }
 }
