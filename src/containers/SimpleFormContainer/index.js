a67984db09b5fcb1accc5ec4b2a71ed7f196679e
brucecham <brucecham@qq.com>
2017-07-13 13:23:52 +0800

    fix commit

--- src/containers/SimpleFormContainer/index.js
+++ src/containers/SimpleFormContainer/index.js
@@ -9,4 +9,3 @@ const AsyncSimpleForm = () => (
 )
 
 export default AsyncSimpleForm
-
--- src/utils/bundle.js
+++ src/utils/bundle.js
@@ -18,29 +18,29 @@ export default class Bundle extends React.Component {
     }
 
     componentWillMount () {
-    this.load(this.props)
-  }
+      this.load(this.props)
+    }
 
     componentWillReceiveProps (nextProps) {
-    if (nextProps.load !== this.props.load) {
-      this.load(nextProps)
+      if (nextProps.load !== this.props.load) {
+        this.load(nextProps)
+      }
     }
-  }
 
     load (props) {
-    this.setState({
-      mod: null
-    })
-    props.load((mod) => {
       this.setState({
-        // handle both es imports and cjs
-        mod: mod.default ? mod.default : mod
+        mod: null
       })
-    })
-  }
+      props.load((mod) => {
+        this.setState({
+        // handle both es imports and cjs
+          mod: mod.default ? mod.default : mod
+        })
+      })
+    }
 
     render () {
-    if (!this.state.mod) { return false }
-    return this.props.children(this.state.mod)
-  }
+      if (!this.state.mod) { return false }
+      return this.props.children(this.state.mod)
+    }
 }
